8de47d961f0108b61c67bc0aac104239605e45a1
PManiAwasthi <awasthiprakashmani7@gmail.com>
2023-01-04 19:38:40 +0530

    exception class handled and bug for logger is solved

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from housing.logger import logging
+from housing.exception import HousingException
+import sys
+
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():
     logging.info("we are testing logging module")
+    try:
+        raise Exception("This is an custom exception test")
+    except Exception as e:
+        logging.info(HousingException(e, sys))
     return "Starting Machine learning project"
 
 
