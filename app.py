e0437c4d78c447ed9e00db3bfa0f4a82b48048b1
PManiAwasthi <awasthiprakashmani7@gmail.com>
2023-01-05 18:53:20 +0530

    data ingestion bug inspection

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ from housing.logger import logging
 from housing.exception import HousingException
 import sys
 from housing.entity.config_entity import DataIngestionConfig
+from housing.pipeline.pipeline import Pipeline
+from housing.config.configuration import Configuration
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():
     logging.info(f"we are testing logging module {DataIngestionConfig}")
     try:
-        raise Exception("This is an custom exception test")
+        # pipeline = Pipeline(config=Configuration())
+        # pipeline.run_pipeline()
+        logging.info(f"Works fine")
     except Exception as e:
         logging.info(HousingException(e, sys))
     return "Starting Machine learning project"
